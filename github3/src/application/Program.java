b994e8d7be31a2197ed7f6074dc4ce4e71c6e038
Daniel Fonseca <danielrdf@gmail.com>
2022-11-15 19:07:07 -0300

    findAll

--- github3/src/application/Program.java
+++ github3/src/application/Program.java
@@ -13,13 +13,19 @@ public class Program {
 
 		AlunoDao alunoDao = DaoFactory.createAlunoDao();
 		
-		System.out.println("=== TEST 1: aluno findById ===");
+		System.out.println("==== TEST 1: aluno findById ====");
 		Aluno aluno = alunoDao.findById(3);
 		System.out.println(aluno);
 		
-		System.out.println("\n=== TEST 2: aluno findByCurso ===");
+		System.out.println("\n==== TEST 2: aluno findByCurso ====");
 		Curso curso = new Curso(2, null);
 		List<Aluno> list = alunoDao.findByCurso(curso);
+		for (Aluno aluno1 : list) {
+			System.out.println(aluno1);
+		}
+		
+		System.out.println("\n==== TEST 3: aluno findAll ====");
+		list = alunoDao.findAll();
 		for (Aluno aluno1 : list) {
 			System.out.println(aluno1);
 		}
--- github3/src/model/dao/impl/AlunoDaoJDBC.java
+++ github3/src/model/dao/impl/AlunoDaoJDBC.java
@@ -90,8 +90,41 @@ public class AlunoDaoJDBC implements AlunoDao {
 
 	@Override
 	public List<Aluno> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT aluno.*, curso.nome as Curso "
+					+ "FROM aluno JOIN curso "
+					+ "ON aluno.idCurso = curso.id "
+					+ "ORDER BY nome");
+			
+			rs = st.executeQuery();
+			
+			List<Aluno> list = new ArrayList<>();
+			Map<Integer, Curso> map = new HashMap<>();
+			
+			while (rs.next()) {
+				
+				Curso curso1 = map.get(rs.getInt("idCurso"));
+				
+				if (curso1 == null) {
+					curso1 = instantiateCurso(rs);
+					map.put(rs.getInt("idCurso"), curso1);
+				}
+				
+				Aluno aluno = instantiateAluno(rs, curso1);
+				list.add(aluno);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
